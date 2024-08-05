a8b44caf36a113c9f8a36703cd7ee675e54eebd9
OkinagaIga <okinagaiga@gmail.com>
2024-08-05 21:45:48 +0300

    Add files via upload

---
+++ L.2.2.py
@@ -0,0 +1,9 @@
+first = 18
+second = 2
+third = 1986
+if first == second == third:
+    print('Bingo!')
+if first == second or second == third or first == third:
+    print('So-so')
+else:
+    print("Oh, nooooo")
